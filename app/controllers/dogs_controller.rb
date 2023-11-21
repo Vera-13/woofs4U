26ac01750aeb3a2a81afdedfb87349ef29909540
Laura Naderimehr <lauranaderimehr@gmail.com>
2023-11-21 11:42:54 +0100

    Edit Controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,12 +4,31 @@ class DogsController < ApplicationController
   end
 
   def create
-    @dog = Dog.new(dog-params)
+    @dog = Dog.new(dog_params)
     if @dog.save
       redirect dog_path(@dog)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      redirect dog_path(@dog)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to dogs_path
   end
 
   private
